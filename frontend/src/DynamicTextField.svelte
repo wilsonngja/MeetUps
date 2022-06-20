f936fd44cb39dd31bff499d75563644ca1a6a094
chinhan99 <braydenyong@yahoo.com.sg>
2022-06-20 22:02:32 +0800

    Revert "fixed thet Button issue"
    This reverts commit 8ef67e59bfc43dcc6ad1e0263f890748746e392d.

--- frontend/src/DynamicTextField.svelte
+++ frontend/src/DynamicTextField.svelte
@@ -16,12 +16,7 @@
   };
 
   let list_of_modules = new Map();
-  const semester = {
-    "sem-1": "semester1",
-    "sem-2": "semester2",
-    "st-i": "specialterm1",
-    "st-ii": "specialterm2",
-  };
+  const semester = { "sem-1": "semester1", "sem-2": "semester2", "st-i": "specialterm1", "st-ii": "specialterm2" };
   const lesson_type = {
     LEC: "Lecture",
     TUT: "Tutorial",
@@ -37,7 +32,7 @@
     Thursday: [["0800", "0800"]],
     Friday: [["0800", "0800"]],
   };
-  async function submitLink() {
+  async function submitLink(){
     //Variable Declaration
     message = "";
     let list_of_modules = new Map();
@@ -59,11 +54,14 @@
       nus_tt_links.push(val);
     }
 
+
     var response;
     (async () => {
       // const ay = "2022-2023"; // Manually keyed in if json file doesnt load.
       const ay = AcademicYear.AY;
+      
 
+      
       //This for-loop is for each links.
       for (let i = 0; i < nus_tt_links.length; i += 1) {
         //Splitting the links into 2 parts. The first part contains the semester while the second part contains the timetable itself
@@ -71,9 +69,7 @@
         //Splitting the timetable into each individual modules
         var each_module_class = each_timetable_module[1].split("&");
 
-        var sem = each_timetable_module[0].match(
-          "(sem-1)|(sem-2)|(st-i)|(st-ii)"
-        )[0];
+        var sem = each_timetable_module[0].match('(sem-1)|(sem-2)|(st-i)|(st-ii)')[0];
         // var = semester[(each_timetable_module[0]).match(/\w+-\w/)[0]];
         //For loop to iterate through each of the modules
         for (let j = 0; j < each_module_class.length; j += 1) {
@@ -94,41 +90,42 @@
           //After all the classes type has been configured into a map, store into the module list where the module list will contain
           //the module code as the key and the map of class type and class code as the value
           module_list.set(splitted_module_slot[0], module_lesson_type);
-          for (var [key, value] of module_lesson_type.entries()) {
+          for (var [key, value] of module_lesson_type.entries())
+          {
+            
+
             // Get the timetable info from the database through the server.
-            const response = await fetch("http://localhost:3000", {
+            const response = await fetch("http://localhost:3000",
+            {
               method: "post",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
+              headers : {
+				        'Accept': 'application/json',
+				        'Content-Type': 'application/json'
+			        },
+              body : JSON.stringify({
                 type: "free slot",
                 module_code: splitted_module_slot[0],
                 class_type: lesson_type[key],
                 class_code: value,
-                semester: semester[sem],
-              }),
+                semester: semester[sem]
+              })
             });
 
-            var data = await response.json();
+            var data = (await response.json());
 
-            for (
-              var class_num = 0;
-              class_num < data["result"].length;
-              class_num += 1
-            ) {
-              lesson_slot[data["result"][class_num]["LessonDay"]].push([
-                data["result"][class_num]["StartTime"],
-                data["result"][class_num]["endTime"],
-              ]);
+            for (var class_num = 0; class_num < data['result'].length; class_num += 1)
+            {
+              (lesson_slot[data['result'][class_num]['LessonDay']]).push([data['result'][class_num]['StartTime'], data['result'][class_num]['endTime']]);
             }
+
           }
+
         }
 
         module_list.clear();
       }
 
+      
       for (const [key, value] of Object.entries(lesson_slot)) {
         //2359 is the end of the search. Can be change to differnt class timing. But take note that the class in NUS ends latest at 9pm
         lesson_slot[key].push(["2359", "2359"]);
@@ -140,14 +137,15 @@
           //Check if there is any slots in between each classes
           if (parseInt(value[i][1]) < parseInt(value[i + 1][0])) {
             //Append the message if there is a free slot
-            message +=
-              key + ": " + value[i][1] + "-" + value[i + 1][0] + "<br/>";
+            message += key + ": " + value[i][1] + "-" + value[i + 1][0] + "<br/>";
             num_timeslots += 1;
           }
         }
       }
+
+
     })();
-  }
+  };
 </script>
 
 <div>
@@ -166,6 +164,11 @@
       placeholder="Enter Timetable Link!"
     />
   </div>
+
+
+
+
+
 {/each}
 
 {#if num_links > 1}
@@ -184,18 +187,19 @@
   <strong>Find Time</strong></button
 >
 
-<div class="freeslot_div" contenteditable="false" bind:innerHTML={message}>
-  <p>{message}</p>
+<div class = "freeslot_div" contenteditable="false" bind:innerHTML={message}>
+  <p>{message}</p> 
 </div>
+
 
 <style>
   button {
-    background: #533a7b;
-    color: white;
-    border: none;
-    font-size: 1em;
-    padding: 8px 12px;
-    border-radius: 2px;
-    text-align: center;
-  }
+		background: #533A7B;
+		color: white;
+	  	border: none;
+		font-size: 1em;
+	  	padding: 8px 12px;
+	  	border-radius: 2px;
+		text-align: center;
+	}
 </style>
